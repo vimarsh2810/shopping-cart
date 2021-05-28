5cbba720418431bd24c616d717dbf7cba7c05ff1
vimarsh <vimarshrathod23@gmail.com>
2021-05-28 19:04:13 +0530

    Minor fixes

--- src/components/shared/pagination.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div v-if="totalPages > 0" class="pagination-wrapper">
-    <span class="pagination-btn" v-if="showPrevious" @click="updatePage(currentPage - 1)"><</span>
-      {{ currentPage }} of {{ totalPages }} pages
-    <span class="pagination-btn" v-if="showNext" @click="updatePage(currentPage + 1)">></span>
-  </div>
-</template>
