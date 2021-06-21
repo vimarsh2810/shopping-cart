da06386b44c78348ae783fc0f9c73f48da331865
vimarsh <vimarshrathod23@gmail.com>
2021-06-21 11:47:44 +0530

    CSS Cleaning

--- src/components/admin/edit-profile.vue
+++ src/components/admin/edit-profile.vue
@@ -91,45 +91,10 @@ export default {
 }
 </script>
 
+<style scoped src="../../assets/css/form.css"></style>
+
 <style scoped>
   .card-1 {
-    max-width: 500px;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 100px;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-  }
-
-  .alert {
-    font-size: 14px !important;
-    padding: 6px 10px !important;
-    margin-bottom: 10px !important;
-  }
-
-  form {
-    padding: 20px;
-  }
-
-  .card {
-    border: none !important;
-  }
-
-  .card-title {
-    margin-bottom: 0;
-    color: white;
-    padding: 20px;
-    background-color: #007bff;
-    border-top-left-radius: .25rem;
-    border-top-right-radius: .25rem;
-  }
-
-  .material-button {
-    box-shadow: 0 2px 4px rgba(0,0,0,0.16), 0 2px 4px rgba(0,0,0,0.23);
-  }
-
-  .form-control:focus,
-  .btn:focus {
-    box-shadow:none !important;
+    margin-top: 100px !important;
   }
 </style>
